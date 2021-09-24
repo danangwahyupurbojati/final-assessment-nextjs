ffa32f7ed651b5ce7ce79e3032fa500608be2aac
danangwahyupurbojati <danang.wahyu@sirclo.com>
2021-09-24 15:29:49 +0700

    add create theme material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000'
+        }
+    }
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     ) 
 }
 
